bffb4bf1eefc8a4903fde231bf6055a6303aa843
GooForSupper <llvandersloot@gmail.com>
2023-06-02 21:07:51 +0200

    shake ya booty

---
+++ Project Vrij I/Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    [SerializeField] AnimationCurve curve;
+
+    [SerializeField] bool start;
+    [SerializeField] float duration = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            start = true;
+        }
+
+        if (start)
+        {
+            start = false;
+            StartCoroutine(Shaking());
+        }
+    }
+
+    IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.position;
+        float elapsedTime = 0;
+        GetComponent<AudioSource>().Play();
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float strength = curve.Evaluate(elapsedTime / duration);
+            transform.position = startPosition + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = startPosition;
+    }
+}
